239575aab55234550260ea39624a890cfac61be6
TKT2016 <bailanzi@aliyun.com>
2016-12-02 10:20:25 +0800

    改进接口

--- ZLOGO开发包/绘图窗体.cs
+++ ZLOGO开发包/绘图窗体.cs
@@ -7,17 +7,17 @@ namespace ZLOGO开发包
     [ZMapping(typeof(TurtleForm))]
     public abstract class 绘图窗体
     {
+        [ZCode("小乌龟")]
         [ZCode("小海龟")]
         public TurtleSprite Turtle { get; set; }
 
         [ZCode("窗口")]
         public TurtleForm Window { get; set; }
 
+        [ZCode("开始绘画")]
+        [ZCode("开始画图")]
         [ZCode("开始绘图")]
         public abstract void RunZLogo();
-        
-        //[ZCode("标题")]
-        //public Form Text { get; set; }
     
         [ZCode("设置标题为(string:title)")]
         public abstract void SetTitle(string title);
--- ZLogoIDE/ZLogoCompiler.cs
+++ ZLogoIDE/ZLogoCompiler.cs
@@ -8,8 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ZCompileCore.Builders;
+using ZCompileCore.Loads;
 using ZCompileCore.Reports;
 using ZLangRT;
+using ZLangRT.Descs;
 using ZLangRT.Utils;
 using ZLogoEngine;
 
@@ -24,26 +26,37 @@ namespace ZLogoIDE
 
         public ProjectCompileResult CompileResult { get; private set;}
 
+        private static TKTProcDesc 开始绘图DESC;
+        private static TKTProcDesc 开始画图DESC;
+        private static TKTProcDesc 开始绘画DESC;
+
+        static ZLogoCompiler()
+        {
+            开始绘图DESC = new TKTProcDesc();
+            开始绘图DESC.Add("开始绘图");
+
+            开始画图DESC = new TKTProcDesc();
+            开始画图DESC.Add("开始画图");
+
+            开始绘画DESC = new TKTProcDesc();
+            开始绘画DESC.Add("开始绘画");
+        }
+
         public ZLogoCompiler(FileInfo zlogoFileInfo)
         {
             srcFileInfo = zlogoFileInfo;
-            /*if (!srcFileInfo.EndsWith(ZLogoExt, StringComparison.OrdinalIgnoreCase))
-            {
-                srcFileInfo +=ZLogoExt;
-            }*/
             projectModel = new ZCompileProjectModel();
             classModel = new ZCompileClassModel();
 
-            projectModel.ProjectRootDirectoryInfo = srcFileInfo.Directory;// AppDomain.CurrentDomain.BaseDirectory;
+            projectModel.ProjectRootDirectoryInfo = srcFileInfo.Directory;
             projectModel.BinaryFileKind = PEFileKinds.Dll;
             projectModel.BinarySaveDirectoryInfo = srcFileInfo.Directory;
             projectModel.ProjectPackageName = "ZLogoIDE";
             projectModel.EntryClassName = Path.GetFileNameWithoutExtension(srcFileInfo.FullName);
-            projectModel.BinaryFileNameNoEx = Path.GetFileNameWithoutExtension(srcFileInfo.FullName);
-            
+            projectModel.BinaryFileNameNoEx = Path.GetFileNameWithoutExtension(srcFileInfo.FullName);     
             projectModel.AddRefPackage("Z语言系统");
             projectModel.AddRefDll(new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ZLOGO开发包.dll")));
-            projectModel.NeedSave = true;
+            projectModel.NeedSave = false;
             classModel.SourceFileInfo = srcFileInfo;
             classModel.PreSourceCode =
 @"
@@ -62,6 +75,21 @@ namespace ZLogoIDE
             ZCompileBuilder builder = new ZCompileBuilder();
             ProjectCompileResult result = builder.CompileProject(projectModel);
             CompileResult= result;
+            CheckStartMethodEnum checkStartResult = CheckStartMethod(result);
+            if (checkStartResult == CheckStartMethodEnum.NoType)
+            {
+                
+            }
+            else if (checkStartResult == CheckStartMethodEnum.NoExists)
+            {
+                CompileMessage compileMessage = new CompileMessage();
+                compileMessage.Text = "程序中不存在'开始绘图'或者'开始画图'或者'开始绘画'过程";
+                result.Errors.Add(compileMessage);
+            }
+            else
+            {
+                
+            }
             return result;
         }
 
@@ -75,21 +103,40 @@ namespace ZLogoIDE
             {
                 return;
             }
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(turtleForm);
-
+ 
             if (CompileResult.CompiledTypes.Count > 0)
             {
                 Type type = CompileResult.CompiledTypes[0];
-                //TurtleForm turtleForm = ReflectionUtil.NewInstance(type) as TurtleForm;
-                //turtleForm.Show();
 
                 using (TurtleForm turtleForm = ReflectionUtil.NewInstance(type) as TurtleForm)
                 {
                     turtleForm.Run();
                 }
             }
+        }
+
+        static CheckStartMethodEnum CheckStartMethod(ProjectCompileResult projectCompileResult)
+        {
+            if (projectCompileResult.CompiledTypes.Count == 0) return CheckStartMethodEnum.NoType;
+            Type type = projectCompileResult.CompiledTypes[0];
+            TktGcl gcl= new TktGcl(type, null);
+            TKTProcDesc searchedProcDesc = null;
+            searchedProcDesc = gcl.SearchProc(开始绘图DESC);
+            if (searchedProcDesc != null) return CheckStartMethodEnum.开始绘图;
+            searchedProcDesc = gcl.SearchProc(开始画图DESC);
+            if (searchedProcDesc != null) return CheckStartMethodEnum.开始画图;
+            searchedProcDesc = gcl.SearchProc(开始绘画DESC);
+            if (searchedProcDesc != null) return CheckStartMethodEnum.开始绘画;
+            return CheckStartMethodEnum.NoExists;
+        }
+
+        enum CheckStartMethodEnum
+        {
+            NoType,
+            NoExists,
+            开始绘图,
+            开始画图,
+            开始绘画
         }
     }
 }
